5586ea7b441b2346ea9992046962950ee6d97e34
afteralec <github email address>
2020-08-14 10:19:53 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get "users/:id", to: "users#show", as: "user"
+  get "pages/about", to: "notes#about"
+  root "notes#index"
+  resources :notes
 end
